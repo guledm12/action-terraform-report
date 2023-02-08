6b789d82f25581f60e64eb6d3a5d5825f65cf5a9
guledm12 <guledmo12@gmail.com>
2023-02-08 09:27:09 -0500

    fixed issue

--- dist/index.js
+++ dist/index.js
@@ -104772,7 +104772,7 @@ function report (data) {
   } = github$1.context;
   const headerText = customHeader;
   data.header = headerText;
-  data.footer = `Action Run: [\`#${runId}\`](https://github.com/${repo.owner}/${repo.repo}/actions/runs/${runId}) from commit: ${sha}`;
+  data.footer = `Action Run: [\`#${runId}\`](https://github.com/${repo.owner}/${repo.repo}/actions/runs/${runId}) - Commit: ${sha}`;
   data.body = `
 ### ${data.header}
 ---
@@ -104793,13 +104793,12 @@ ${textContent}
 
   if (data.showDiff === 'true') {
     const diff = patches.map(patch => `\`\`\`diff\n${patch}\n\`\`\``).join('\n\n');
-    data.body += `<details><summary>Show Diff</summary> ${diff} </details>`;
+    data.body += `<details><summary>Show Diff</summary>
+
+${diff}
+</details>
+`;
   }
-//   if (patches.map(patch => `\`\`\`diff\n${patch}\n\`\`\``).join('\n\n') == undefined || null || ""){
-// 	data.body += `<p>No differences made</p>`
-//   }
-//   data.body += `</details>`
- 
 
   data.body += `
 ---
