abad219b106ee354f975494ba873fd3377bb3187
guledm12 <guledmo12@gmail.com>
2023-02-08 10:35:57 -0500

    fixed issue

--- dist/index.js
+++ dist/index.js
@@ -104772,7 +104772,7 @@ function report (data) {
   } = github$1.context;
   const headerText = customHeader
   data.header = headerText;
-  data.footer = `Action: [Click here to view action run](https://github.com/${repo.owner}/${repo.repo}/actions/runs/${runId}) \n Commit: ${sha}`;
+  data.footer = `[Click to view action run](https://github.com/${repo.owner}/${repo.repo}/actions/runs/${runId}) \n [Click to view commit](${sha})`;
   data.body = `
 ### ${data.header}
 ---
